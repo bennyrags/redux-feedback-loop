5a303fe4fbcbd9969a9d95ae5be85feb9e198db3
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-05 17:37:43 -0500

    review feedback

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import HowFeelingToday from '../HowFeelingToday/HowFeelingToday';
+import ReviewFeedback from '../ReviewFeedback/ReviewFeedback'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <HowFeelingToday />
+        <ReviewFeedback />
       </div>
     );
   }
---
+++ src/components/ReviewFeedback/ReviewFeedback.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+class ReviewFeedback extends Component {
+
+    render() {
+        return(
+            <section>
+            <h2>
+                Review Your Feedback
+            </h2>
+
+            <h3>Feelings:</h3>
+            <h3>Understanding:</h3>
+            <h3>Support:</h3>
+            <h3>Comments</h3>
+            </section>
+        )
+    }
+
+}
+
+export default ReviewFeedback;
