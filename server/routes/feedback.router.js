2c7f5b2cda0a3225a0253a38a064d1543694cc88
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-05 17:09:06 -0500

    sql thing set up

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
 router.get('/', (req,res) => {
     res.send('Hello from the feedback router!');
+    sqlText = `SELECT * FROM feedback`
+    pool.query(sqlText)
+    .then(result=> {
+        res.send(result.rows);
+
+    })
 })
 
 
