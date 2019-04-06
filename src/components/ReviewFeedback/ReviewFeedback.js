d81d2394cf310df7819a730631d90ed2667c51ce
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-06 13:59:51 -0500

    button chg wrking - see reviewfeedback

--- src/components/ReviewFeedback/ReviewFeedback.js
+++ src/components/ReviewFeedback/ReviewFeedback.js
@@ -4,6 +4,37 @@ import './ReviewFeedback.css'
 class ReviewFeedback extends Component {
 
 
+    //need to make a func that looks to make sure all fields are complete
+    //enables button
+    //changes text from incomplete to submit
+    //allows user to click on button to submit feedback
+
+    //OR 
+
+    //if URL has the string of the ReviewFeedback, we know the fields are filled and its time to submit
+
+    //ok so how do we change the button? let's start with that
+
+    changeButton = () => {
+        console.log(`changeButton func fired on componentDidMount`);
+        let submitButton = document.getElementById('submit');
+        console.log(`submit button`, submitButton)
+        let hash = window.location.hash;
+        console.log('window location pathname:', window.location.hash)
+        
+        if (hash !== '#/ReviewFeedback') {
+            submitButton.setAttribute('disabled', 'true');
+            submitButton.innerHTML='Incomplete'
+    }       
+        else {
+            
+        }
+    }
+
+    componentDidMount() {
+        this.changeButton()
+
+    }
 
 
     render() {
@@ -19,11 +50,11 @@ class ReviewFeedback extends Component {
             <h3>Support: {this.props.reduxState.reducer1.supported}</h3>
             <h3>Comments: {this.props.reduxState.reducer1.comments}</h3>
             
-            <button  disabled>Incomplete</button>
+            <button id='submit'  onClick={this.props.submitFeedback} >Submit Feedback</button>
 
-            <button id='submit' onClick={this.props.submitFeedback}>Submit Feedback</button>
-            </section>
-
+            {/* <button id='submit' onClick={this.props.submitFeedback}>Submit Feedback</button>
+            </section> */}
+    </section>
         )
     }
 
