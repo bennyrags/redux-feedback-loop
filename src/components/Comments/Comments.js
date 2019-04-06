e2ac08b4667e92a04fbb436128122382d0d0a935
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-05 21:23:04 -0500

    comments added, before trying to figure out review page

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -6,17 +6,36 @@ class Comments extends Component {
         comments: ''
     }
 
-    render() {
-        return(
-            <section>
-                Comments
-            </section>
-        )
-
+    handleClick = (e) => {
+        console.log(`this is state from  comments handleClick`, this.state);
+        this.props.dispatch({type: 'COMMENTS', payload: this.state.comments});
+       // this.props.history.push(`/comments`);
     }
 
+    handleChange = (e) => {
+        this.setState({
+            comments: e.target.value,
+        })
+        console.log(`this is state from  comments handleChange`, this.state);
 
+    }
+        render() {
+        return(
+            <section>
+                <h1>
+                    Are You feeling supported today
+                </h1>
+                <label>Comments?</label>
+                <input type="text" onChange={this.handleChange} value={this.state.comments}/>
+ 
+                <button onClick={this.handleClick}>Next</button>
+            </section>
+        )
+    }
 }
 
+const mapReduxStateToProps = reduxState => ({
+    reduxState,
+})
 
-export default Comments;
+export default connect(mapReduxStateToProps)(Comments);
